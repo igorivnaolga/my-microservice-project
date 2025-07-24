a995c0c8766f9a29c1d6261065cfb878bc1d5539
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2025-07-24 13:27:53 +0300

    fix bugs

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -18,6 +18,10 @@ output "internet_gateway_id" {
   value       = aws_internet_gateway.main.id
 }
 
+output "vpc_cidr_block" {
+  value = aws_vpc.main.cidr_block
+}
+
 #output "nat_gateway_id" {
 #  description = "ID NAT-інстансу"
 #  value       = aws_nat_gateway.main.id
