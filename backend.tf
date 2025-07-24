84761e4ebfdddd53381a46d1279179e48d487b46
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2025-07-24 12:21:46 +0300

    add rds module

--- backend.tf
+++ backend.tf
@@ -1,9 +1,12 @@
+# Uncomment this to connect backend with Terraform
+
 #terraform {
 #  backend "s3" {
-#   bucket          = "terraform-bucket-lesson-5"
-#    key            = "lesson-5/terraform.tfstate"
-#    region         = "ap-southeast-2"
-#    use_lockfile   = true
+#   bucket          = "terraform-bucket-567832104967"
+#   key            = "terraform.tfstate"
+#   region         = "ap-southeast-2"
+#   use_lockfile   = true
+#   encrypt        = true 
 #  }
 #}
 
